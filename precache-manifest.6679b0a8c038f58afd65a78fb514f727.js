b82167508c5fcef1f30a11b9a736cf71e977953d
sebastian-raubach <sebastian-raubach@users.noreply.github.com>
2022-02-18 16:48:32 +0000

    Deploying to gh-pages from @ cropgeeks/humbug@e4d9a9ca3050ef6e6bf5835b14b7bdde0de5602c 🚀

---
+++ precache-manifest.6679b0a8c038f58afd65a78fb514f727.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4ca0e8a4e9e5b1ad9c747c9fb48892",
+    "url": "android-chrome-192x192.png"
+  },
+  {
+    "revision": "7dfb865b206547758e136cba05b1cff7",
+    "url": "android-chrome-512x512.png"
+  },
+  {
+    "revision": "ac875820e084956f65f907f6e9e30d4e",
+    "url": "apple-touch-icon.png"
+  },
+  {
+    "revision": "61bfd064535af0c276bb63b3fd579733",
+    "url": "browserconfig.xml"
+  },
+  {
+    "revision": "d928dfd0eedded4898b9",
+    "url": "css/about.4120dafe.css"
+  },
+  {
+    "revision": "f14e0e5ccc665c2a8131",
+    "url": "css/app.7f5fe783.css"
+  },
+  {
+    "revision": "32a2515208cbff5172f3",
+    "url": "css/home.726dc5b6.css"
+  },
+  {
+    "revision": "985ef45b73479cd48311e5e02fe2c0ba",
+    "url": "favicon-16x16.png"
+  },
+  {
+    "revision": "724faab4a31b83203599c913dc465d5c",
+    "url": "favicon-32x32.png"
+  },
+  {
+    "revision": "5349b785dbc1603059537ec439176979",
+    "url": "img/hutton.5349b785.svg"
+  },
+  {
+    "revision": "c1d6856e5ba91bed5a67f90bb3725faf",
+    "url": "img/ics-sdg.c1d6856e.svg"
+  },
+  {
+    "revision": "89d0d56de425a28d7938f25fcd46b5ad",
+    "url": "img/logo.89d0d56d.svg"
+  },
+  {
+    "revision": "0d8b7a5666a57d001829faf60f9ac76f",
+    "url": "index.html"
+  },
+  {
+    "revision": "d928dfd0eedded4898b9",
+    "url": "js/about.04876731.js"
+  },
+  {
+    "revision": "f14e0e5ccc665c2a8131",
+    "url": "js/app.a7d20db3.js"
+  },
+  {
+    "revision": "c7b1337e21cf696041d9",
+    "url": "js/chunk-vendors.af3db5aa.js"
+  },
+  {
+    "revision": "32a2515208cbff5172f3",
+    "url": "js/home.506d9840.js"
+  },
+  {
+    "revision": "6d040394f4a51760f922b0f426b00d38",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "2db206e67a3f2036f6b31038b281d1b1",
+    "url": "mstile-144x144.png"
+  },
+  {
+    "revision": "b6eebee438ac41343d89382447df6e16",
+    "url": "mstile-150x150.png"
+  },
+  {
+    "revision": "478585199e7ce5c07eb4ff90a486f472",
+    "url": "mstile-310x150.png"
+  },
+  {
+    "revision": "d2d57642b7b7b37ac348104dbdc53ae6",
+    "url": "mstile-310x310.png"
+  },
+  {
+    "revision": "67765f491337d9a13e68d94ace418ef5",
+    "url": "mstile-70x70.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "2040348cc9c2ac39b61c45139d98408b",
+    "url": "safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b9aa277fcfc34c31db6c7a7ea3469b8c",
+    "url": "site.webmanifest"
+  }
+]);
