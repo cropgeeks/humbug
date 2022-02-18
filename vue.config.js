4bc627fe3dd5915cf5911b8ad0066802c1d6c856
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2022-02-18 17:14:57 +0000

    - ADD: Added PWA stuff.

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,17 @@
 // 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  pwa: {
+    name: 'Humbug',
+    themeColor: '#273c75',
+    msTileColor: '#2b5797',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    start_url: '/index.html',
+    workboxOptions: {
+      skipWaiting: true
+    }
+  }
   // configureWebpack: {
   //   plugins: [
   //     new BundleAnalyzerPlugin()
